946829a809474dfa73b12a2c3caccf6973524897
DataGen64 <bkri0003@student.monash.edu>
2019-08-21 04:13:16 +1000

    reinitialized files

---
+++ FIT5032_Week03/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FIT5032_Week03.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            HelloWorld.Hello hello = new HelloWorld.Hello();
+            ViewBag.Message = hello.GetHello();
+
+            Excersice.ExampleDictionary ed = new Excersice.ExampleDictionary();
+            ed.ExampleDict();
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ FIT5032_Week03/Excersice/Student.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace FIT5032_Week03.Excersice
+{
+
+
+    public class Student
+    {
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public Student(string name, string phoneNumber)
+        {
+            Name = name;
+            PhoneNumber = phoneNumber;
+        }
+
+    }
+
+    public class ExampleDictionary
+    {
+        public void ExampleDict()
+        {
+            Dictionary<int, Student> studentdictionary = new Dictionary<int, Student>();
+
+            Student s1 = new Student("Bharath", "0401175105");
+            Student s2 = new Student("Nirosha", "0445889226");
+
+            studentdictionary.Add(1, s1);
+            studentdictionary.Add(2, s2);
+
+            Student result = new Student("", "");
+
+            studentdictionary.TryGetValue(1, out result);
+
+        }
+
+
+
+
+
+    }
+}
